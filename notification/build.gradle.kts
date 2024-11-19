6e9562249e5481a6d25b88746928b3b1245e4464
Dmitry Volkov <99538385+Avdors@users.noreply.github.com>
2024-11-19 13:14:41 +0100

    add google-services.json and FirebaseNotificationService

--- notification/build.gradle.kts
+++ notification/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.kotlin.compose)
 }
 
 android {
@@ -40,4 +42,19 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    implementation(libs.firebase.messaging.ktx)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    implementation(platform(libs.koin.bom))
+    implementation(libs.koin.core)
+    implementation(libs.koin.core.coroutines)
+    implementation(libs.koin.core.compose)
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
+    implementation(platform(libs.androidx.compose.bom))
+
+    implementation(platform(libs.firebase.bom))
+
 }
